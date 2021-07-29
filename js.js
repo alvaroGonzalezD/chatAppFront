4c611a9ed5a493aa69f1690c495bdc59c69dee12
Alvaro <aglez7412@gmail.com>
2021-07-29 12:56:08 +0200

    Codestyle

--- js.js
+++ js.js
@@ -5,41 +5,40 @@ let direccion = "http://192.168.1.199"
 
 
 function addMessages(msg_id, v) {
-    // tempHtml = $("#textTemplate").html()
+    function getHumantime() {
+        const dateObject = new Date(dateTime)
+
+        const ht = {
+            h: dateObject.getHours(),
+            m: dateObject.getMinutes(),
+            s: dateObject.getSeconds()
+        }
+
+        for (key in ht) {
+            ht[key] = (ht[key] < 10 ? '0' : '') + ht[key]
+        }
+
+        return `${ht.h}:${ht.m}:${ht.s}`
+    }
+
+    function userSide() {
+
+        let msgToAppend = newMsg
+        let usuarioActual = document.getElementById("fname").value
+
+        if (user.toUpperCase() == usuarioActual.toUpperCase()) {
+            msgToAppend = ownMsg
+        }
+
+        $(".speech-wrapper").append(msgToAppend)
+    }
+
     const txt = this["txt"]
     const user = this["user"]
     const dateTime = this["datetime"]
     const id_msg = this["id_msg"]
+    const humanTime = getHumantime()
 
-    // let dateTimeParts = dateTime.split(/[- :]/); // regular expression split that creates array with: year, month, day, hour, minutes, seconds values
-    // dateTimeParts[1]--; // monthIndex begins with 0 for January and ends with 11 for December so we need to decrement by one
-
-    // const dateObject = new Date(...dateTimeParts); // our Date object
-    // const humanTime = dateObject.toLocaleString("es-ES")
-
-    const dateObject = new Date(dateTime)
-
-    // const humanTime = (dateObject.getHours() + ':' + dateObject.getMinutes() + ':' + dateObject.getSeconds())
-    const humanTime =
-        (
-            (
-                (dateObject.getHours() < 10 ? '0' : '') +
-                dateObject.getHours()
-
-            ) + ':' +
-            (
-                (dateObject.getMinutes() < 10 ? '0' : '') +
-                dateObject.getMinutes()
-            ) +
-            ':' +
-            (
-                (dateObject.getSeconds() < 10 ? '0' : '') +
-                dateObject.getSeconds()
-            )
-        )
-
-
-    // const newMsg = `<p class= "mensajeEnChat" id="${msg_id}"><span>${user}: ${txt}</span></p>`
     const newMsg = `
         <div class="bubble" id="${id_msg}">
             <div class="txt">
@@ -48,6 +47,7 @@ function addMessages(msg_id, v) {
                 <span class="timestamp">${humanTime}</span>
             </div>
         <div class="bubble-arrow"></div>`
+
     const ownMsg = `
         <div class="bubble alt" id="${id_msg}">
             <div class="txt">
@@ -57,70 +57,54 @@ function addMessages(msg_id, v) {
         </div>
         <div class="bubble-arrow alt"></div>`
 
-    function userSide() {
-
-        let msgToAppend = newMsg
-        //let msgToAppend = (Math.random() >= 0.5 ? newMsg : ownMsg)
-        let usuarioActual = document.getElementById("fname").value
-        if (user.toUpperCase() == usuarioActual.toUpperCase()) {
-            msgToAppend = ownMsg
-        }
-
-        $(".speech-wrapper").append(msgToAppend)
-    }
     userSide()
-
 }
 
-function scrollToBottom() {
-    var objDiv = document.getElementById("speech-wrapper");
-    objDiv.scrollTop = objDiv.scrollHeight;    
-}
 
 function recibirMensajes() {
 
-    if (document.getElementsByClassName("bubble").length > 0){
-        msg_id = document.getElementsByClassName("bubble")[document.getElementsByClassName("bubble").length-1].getAttribute("id")
-    }else{
-        msg_id = 0
+    function onNewmessages(data) {
+
+        function scrollToBottom() {
+            var objDiv = document.getElementById("speech-wrapper");
+            objDiv.scrollTop = objDiv.scrollHeight;
+        }
+
+        let speechWrapper = document.getElementById("speech-wrapper");
+        let toTheBottom = false
+        if (Math.round(speechWrapper.scrollHeight - speechWrapper.scrollTop) == Math.round(speechWrapper.clientHeight)) {
+            toTheBottom = true
+        }
+
+        $.each(data, function(index) {
+            $.each(this, addMessages);
+        });
+
+        if (toTheBottom) {
+            scrollToBottom()
+        }
     }
 
-    
 
-    datosJsonRecibir = {
-        id_msg: msg_id,
-    }
+    var bubble = document.getElementsByClassName("bubble")
+    let msg_id = (bubble.length > 0) ? bubble[bubble.length - 1].getAttribute("id") : 0
 
     $.ajax({
         url: direccion + "/recibir",
         type: "POST",
         contentType: "application/json charset=utf-8",
         dataType: 'json',
-        data: JSON.stringify(datosJsonRecibir),
+        data: JSON.stringify({id_msg: msg_id}),
         crossDomain: true,
         cache: false,
-        headers: {
-            'Access-Control-Allow-Origin': "*" //direccion + "/recibir"
-        },
         success: function(data) {
-            let speechWrapper = document.getElementById("speech-wrapper");
-            let toTheBottom = false
-            if (Math.round(speechWrapper.scrollHeight-speechWrapper.scrollTop) == Math.round(speechWrapper.clientHeight)){
-                toTheBottom = true
-            }
-
-            //$("#speech-wrapper").html("");
-            $.each(data, function(index) {
-                $.each(this, addMessages);
-            });
-
-            if (toTheBottom) {
-                scrollToBottom()
-            }
+            onNewmessages(data)
         },
-        error: function() {
-            console.log("error")
-        }
+        error: function(req, textStatus, errorThrown) {
+            mensaje.value = ""
+            console.error(textStatus)
+            console.log(errorThrown)
+        },
     });
 
 }
@@ -161,18 +145,15 @@ window.onload = function() {
             type: "POST",
             contentType: "application/json charset=utf-8",
             data: JSON.stringify(datosJson),
-            dataType: 'json',
             crossDomain: true,
             cache: false,
-            headers: {
-                "Access-Control-Allow-Origin": "*" //direccion + "/enviar"
-            },
             success: function(data, textStatus, req) {
-                console.log("todo guay")
+                // pass
             },
             error: function(req, textStatus, errorThrown) {
                 mensaje.value = ""
-                console.log(textStatus)
+                console.error(textStatus)
+                console.log(errorThrown)
             },
             complete: function(req, textStatus) {
                 mensaje.value = ""
